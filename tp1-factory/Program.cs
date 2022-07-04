77e4e497eb7d9999e207926c514e6914fdb15d13
François ROULLAUD <froullaud@positivethinking.tech>
2022-07-04 09:49:32 +0200

    Initialisation repo

---
+++ tp1-factory/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+/*
+    Classe Catalogue : classer véhicules entre essence / éléctrique
+    Classe abstraite + sous-classes concrètes
+    Éviter de modifier la classe catalogue à chaque nouveau type de véhicule grâce à une abstract factory -> dé-résponsabiliser la classe mère
+    L'interface comprend la signature des méthodes qui serviront à définir chaque produit
+        -> Signature = nom + IO (entrées/sorties) 
+        -> // Prototype du C
+*/
+
